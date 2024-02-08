a1aeed770428f840cabadd8392da665784ecbe94
Muayad Haddad <110057430+HashDevTeam@users.noreply.github.com>
2024-02-08 11:44:11 -0500

    feat: built structure

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // You can chnage colors depends 
+  static const Color primary = Colors.white;
+
+}
---
+++ lib/core/utils/app_string.dart
@@ -0,0 +1,4 @@
+class AppStrings {
+  static const String appName = "Fashion Fusion";
+  static const String fontFamily = "";
+}
