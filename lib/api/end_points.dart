f2b0ffe49396ccb6ec0ea411a448f2db106e2099
Adrian Mark Dalipe <50126402+adrnmrk@users.noreply.github.com>
2024-03-01 22:20:11 -0500

    Update end_points.dart

--- lib/api/end_points.dart
+++ lib/api/end_points.dart
@@ -26,4 +26,18 @@ class EndPoints {
   static const String getFavoriteItems = favoriteResourcePath;
   static const String putFavoriteItems = favoriteResourcePath;
   static const String deleteFavoriteItems = favoriteResourcePath;
+
+  // Carts
+  static const String cartResourcePath = '$baseUrl/cart/items';
+  static const String putCartItems = cartResourcePath;
+  static const String getCartItems = cartResourcePath;
+  static const String deleteCartItems = cartResourcePath;
+  static const String deleteCartItemsById = '$cartResourcePath/:productId';
+
+  // Orders
+  static const String postCheckout = '$baseUrl/checkout';
+  static const String orderResourcePath = '$baseUrl/orders';
+  static const String getOrdersById = '$orderResourcePath/:orderId';
+  static const String patchOrdersById = '$orderResourcePath/:orderId';
+
 }
