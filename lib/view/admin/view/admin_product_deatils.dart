f6a0f24c37914bb746bac46b949e33c8eaadddfe
muayadhaddad59 <muayadhaddad@yahoo.com>
2024-03-07 20:08:51 -0500

    feat: product details

--- lib/view/admin/view/admin_product_deatils.dart
+++ lib/view/admin/view/admin_product_deatils.dart
@@ -1,5 +1,8 @@
+import 'package:fashion_fusion/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:fashion_fusion/data/product/model/product_model.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AdminProductDetailsScreen extends StatelessWidget {
   final ProductModel model;
@@ -9,55 +12,117 @@ class AdminProductDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.bg,
       appBar: AppBar(
         title: const Text("Product Details"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              model.productName!,
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Category: ${model.category}",
-              style: const TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Description: ${model.productDescription}",
-              style: const TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Price: \$${model.price}",
-              style: const TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Sold Quantity: ${model.soldQuantity}",
-              style: const TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Tags: ${model.tags}",
-              style: const TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Created At: ${model.createdAt}",
-              style: const TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Updated At: ${model.updatedAt}",
-              style: const TextStyle(fontSize: 16),
+            _cursolImages(),
+            Padding(
+              padding: const EdgeInsets.all(15.0).w,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'H&M',
+                        style: TextStyle(
+                          color: const Color(0xFF222222),
+                          fontSize: 22.sp,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const Spacer(),
+                      Text(
+                        '\$${model.price}',
+                        textAlign: TextAlign.right,
+                        style: const TextStyle(
+                          color: Color(0xFF222222),
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                    ],
+                  ),
+                  Text(
+                    model.category ?? "",
+                    style: const TextStyle(
+                      color: Color(0xFF9B9B9B),
+                      fontSize: 11,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "${model.productDescription}",
+                    style: const TextStyle(
+                      color: Color(0xFF222222),
+                      fontSize: 14,
+                      fontFamily: 'Metropolis',
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: -0.15,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "Sold Quantity: ${model.soldQuantity}",
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "Tags: ${model.tags}",
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "Created At: ${model.createdAt}",
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "Updated At: ${model.updatedAt}",
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  SizedBox _cursolImages() {
+    return SizedBox(
+      width: double.infinity,
+      child: FlutterCarousel(
+        options: CarouselOptions(
+          viewportFraction: 1,
+          showIndicator: true,
+          slideIndicator: const CircularSlideIndicator(),
+        ),
+        items: model.images?.map((i) {
+          final image = "http://127.0.0.1:3000/products/images/$i";
+
+          return Builder(
+            builder: (BuildContext context) {
+              return SizedBox(
+                width: 1.sw,
+                child: Image.network(
+                  image,
+                  width: double.infinity,
+                  alignment: Alignment.topCenter,
+                  fit: BoxFit.cover,
+                ),
+              );
+            },
+          );
+        }).toList(),
       ),
     );
   }
