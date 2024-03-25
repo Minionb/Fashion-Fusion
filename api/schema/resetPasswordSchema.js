5f631b6a27d62ff737504a0c543b569a73df9430
Adrian Mark Dalipe <50126402+adrnmrk@users.noreply.github.com>
2024-03-24 22:28:13 -0400

    Reset password update

--- api/schema/resetPasswordSchema.js
+++ api/schema/resetPasswordSchema.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-const resetTokenSchema = new mongoose.Schema({
+const resetPasswordSchema = new mongoose.Schema({
   customerId: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
   },
-  token: { type: String, required: true },
   email: { type: String, required: true },
+  password: { type: String, required: true },
   createdAt: { type: Date, expires: "1d", default: Date.now },
 });
 
-module.exports = mongoose.model("resetToken", resetTokenSchema);
+module.exports = mongoose.model("resetPassword", resetPasswordSchema);
