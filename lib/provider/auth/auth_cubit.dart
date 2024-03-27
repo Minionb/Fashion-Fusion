8c893caca8570d79575d38150ddc2fec731ccda1
muayadhaddad59 <muayadhaddad@yahoo.com>
2024-03-27 15:10:23 -0400

    feat: added logout button

--- lib/provider/auth/auth_cubit.dart
+++ lib/provider/auth/auth_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:fashion_fusion/core/utils/app_service.dart';
 import 'package:fashion_fusion/data/auth/model/login_model.dart';
 import 'package:fashion_fusion/data/auth/model/signup_model.dart';
 import 'package:fashion_fusion/data/auth/usecase/login_usecase.dart';
@@ -5,13 +6,17 @@ import 'package:fashion_fusion/data/auth/usecase/register_usecase.dart';
 import 'package:fashion_fusion/data/auth/usecase/reset_password_usecase.dart';
 import 'package:fashion_fusion/provider/states/cubit_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthCubit extends Cubit<CubitState> {
   final LoginUsecase loginUsecase;
   final RegisterUsecase registerUsecase;
   final ResetPasswordUsecase resetPasswordUsecase;
 
-  AuthCubit({required this.registerUsecase, required this.loginUsecase, required this.resetPasswordUsecase})
+  AuthCubit(
+      {required this.registerUsecase,
+      required this.loginUsecase,
+      required this.resetPasswordUsecase})
       : super(Initial());
 
   void login(LoginModel model) async {
@@ -34,5 +39,10 @@ class AuthCubit extends Cubit<CubitState> {
     emit(response.fold((l) => DataFailure(errorMessage: l.toString()),
         (r) => DataSuccess(data: r)));
   }
-  
+
+  void signOut() async {
+    emit(DataLoading());
+    await sl<SharedPreferences>().clear();
+    emit(DataSuccess());
+  }
 }
