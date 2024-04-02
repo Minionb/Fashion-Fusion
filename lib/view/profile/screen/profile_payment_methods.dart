35876bd8deac4556621658c5c739a35c317a1908
Adrian Mark Dalipe <50126402+adrnmrk@users.noreply.github.com>
2024-04-01 20:25:35 -0400

    Update profile_payment_methods.dart
    Format

--- lib/view/profile/screen/profile_payment_methods.dart
+++ lib/view/profile/screen/profile_payment_methods.dart
@@ -20,90 +20,96 @@ class ProfilePaymentMethods extends StatelessWidget {
   List<PaymentModel> newPaymentMethodsList = [];
 
   ProfilePaymentMethods({
-    super.key, 
+    super.key,
     required this.paymentMethodsList,
   });
 
   @override
   Widget build(BuildContext context) {
     return HelperMethod.loader(
-      child: Scaffold(
-        appBar: AppBar(title: const Text("Payment methods"),),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column( crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text("Your payment cards", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
-              /// Cards of entered payment methods
-              const Padding(padding: EdgeInsets.all(5)),
-              // BlocBuilder<ProfileCubit, ProfileState>(
-              //   builder: (context, state) {
-              //     if (state is ProfileIsLoadingState) {
-              //       print("Profile LOADING");
-              //       context.loaderOverlay.show();
-              //     }
-              //     if (state is ProfileLoadedState) {
-              //       context.loaderOverlay.hide();
-              //       if (state.model!.payments!.isNotEmpty) {
-              //         paymentMethodsList = state.model!.payments;
-              //         return 
-              //       }
-              //       else {
-              //         return Text("No saved payment methods");
-              //       }
-              //     }
-              //   }
-              // ),
-              //Stack(
-                //children: [
-              paymentMethodsList.isNotEmpty ? 
-                Expanded(
-                  child: 
-                  Stack(
-                    children: [
-                      ListView.builder(
-                        itemCount: paymentMethodsList.length,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: PaymentOptionsCard(paymentMethod: paymentMethodsList[index])
-                          );
-                        }
-                      ),
-                      Positioned(
-                        bottom: 16.0,
-                        right: 16.0,
-                        child: FloatingActionButton(onPressed: () {
-                            toAddPaymentMethod(context, paymentMethodsList);
-                          },
-                          child: const Text("+", style: TextStyle(fontSize: 25)),
-                        ),
-                      )
-                    ],
-                  )
-                )
-              : 
-              Stack(
+        child: Scaffold(
+            appBar: AppBar(
+              title: const Text("Payment methods"),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Center(
-                    child: Text("No saved payment methods")
-                  ),
-                  Positioned(
-                    bottom: 16.0,
-                    right: 16.0,
-                    child: FloatingActionButton(onPressed: () {
-                        toAddPaymentMethod(context, paymentMethodsList);
-                      },
-                      child: const Text("+", style: TextStyle(fontSize: 25)),
-                    ),
-                  )
+                  const Text("Your payment cards",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+
+                  /// Cards of entered payment methods
+                  const Padding(padding: EdgeInsets.all(5)),
+                  // BlocBuilder<ProfileCubit, ProfileState>(
+                  //   builder: (context, state) {
+                  //     if (state is ProfileIsLoadingState) {
+                  //       print("Profile LOADING");
+                  //       context.loaderOverlay.show();
+                  //     }
+                  //     if (state is ProfileLoadedState) {
+                  //       context.loaderOverlay.hide();
+                  //       if (state.model!.payments!.isNotEmpty) {
+                  //         paymentMethodsList = state.model!.payments;
+                  //         return
+                  //       }
+                  //       else {
+                  //         return Text("No saved payment methods");
+                  //       }
+                  //     }
+                  //   }
+                  // ),
+                  //Stack(
+                  //children: [
+                  paymentMethodsList.isNotEmpty
+                      ? Expanded(
+                          child: Stack(
+                          children: [
+                            ListView.builder(
+                                itemCount: paymentMethodsList.length,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: PaymentOptionsCard(
+                                          paymentMethod:
+                                              paymentMethodsList[index]));
+                                }),
+                            Positioned(
+                              bottom: 16.0,
+                              right: 16.0,
+                              child: FloatingActionButton(
+                                onPressed: () {
+                                  toAddPaymentMethod(
+                                      context, paymentMethodsList);
+                                },
+                                child: const Text("+",
+                                    style: TextStyle(fontSize: 25)),
+                              ),
+                            )
+                          ],
+                        ))
+                      : Stack(
+                          children: [
+                            const Center(
+                                child: Text("No saved payment methods")),
+                            Positioned(
+                              bottom: 16.0,
+                              right: 16.0,
+                              child: FloatingActionButton(
+                                onPressed: () {
+                                  toAddPaymentMethod(
+                                      context, paymentMethodsList);
+                                },
+                                child: const Text("+",
+                                    style: TextStyle(fontSize: 25)),
+                              ),
+                            )
+                          ],
+                        ),
                 ],
               ),
-            ],
-          ),
-        )
-      )
-    );
+            )));
   }
 }
 
@@ -111,7 +117,7 @@ class PaymentOptionsCard extends StatelessWidget {
   final PaymentModel paymentMethod;
 
   const PaymentOptionsCard({
-    super.key, 
+    super.key,
     required this.paymentMethod,
   });
 
@@ -127,15 +133,21 @@ class PaymentOptionsCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(8.0),
           gradient: LinearGradient(
             colors: [
-              paymentMethod.method == 'VISA' ? AppColors.visaPrimary 
-                : paymentMethod.method == 'Mastercard' ? AppColors.mastercardPrimary 
-                  : AppColors.americanEPrimary,
-              paymentMethod.method == 'VISA' ? AppColors.visaSecondary
-                : paymentMethod.method == 'Mastercard' ? AppColors.mastercardSecondary
-                  : AppColors.americanESecondary,
-              paymentMethod.method == 'VISA' ? AppColors.visaPrimary 
-                : paymentMethod.method == 'Mastercard' ? AppColors.mastercardPrimary 
-                  : AppColors.americanEPrimary,
+              paymentMethod.method == 'VISA'
+                  ? AppColors.visaPrimary
+                  : paymentMethod.method == 'Mastercard'
+                      ? AppColors.mastercardPrimary
+                      : AppColors.americanEPrimary,
+              paymentMethod.method == 'VISA'
+                  ? AppColors.visaSecondary
+                  : paymentMethod.method == 'Mastercard'
+                      ? AppColors.mastercardSecondary
+                      : AppColors.americanESecondary,
+              paymentMethod.method == 'VISA'
+                  ? AppColors.visaPrimary
+                  : paymentMethod.method == 'Mastercard'
+                      ? AppColors.mastercardPrimary
+                      : AppColors.americanEPrimary,
             ],
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
@@ -149,41 +161,32 @@ class PaymentOptionsCard extends StatelessWidget {
             const SizedBox(height: 8.0),
             Row(
               children: [
-                const Expanded(
-                  child: Spacer()
-                ),
-                paymentMethod.method == 'VISA' ?  Image.asset(
-                  "assets/icons/visa.256x79.png",
-                  width: 75,
-                  height: 30,
-                  fit: BoxFit.fill
-                )
-                : paymentMethod.method == 'Mastercard' ? 
-                Image.asset(
-                  "assets/icons/mastercard.256x198.png",
-                  width: 55,
-                  height: 40,
-                  fit: BoxFit.fill
-                )
-                : 
-                Image.asset(
-                  "assets/icons/american-express.256x84.png",
-                  width: 75,
-                  height: 30,
-                  fit: BoxFit.fill
-                ),
+                const Expanded(child: Spacer()),
+                paymentMethod.method == 'VISA'
+                    ? Image.asset("assets/icons/visa.256x79.png",
+                        width: 75, height: 30, fit: BoxFit.fill)
+                    : paymentMethod.method == 'Mastercard'
+                        ? Image.asset("assets/icons/mastercard.256x198.png",
+                            width: 55, height: 40, fit: BoxFit.fill)
+                        : Image.asset(
+                            "assets/icons/american-express.256x84.png",
+                            width: 75,
+                            height: 30,
+                            fit: BoxFit.fill),
               ],
             ),
             15.verticalSpace,
             FittedBox(
               fit: BoxFit.scaleDown,
-              child: Text("**** **** **** ${paymentMethod.cardNumber.substring(paymentMethod.cardNumber.length - 4)}",
+              child: Text(
+                "**** **** **** ${paymentMethod.cardNumber.substring(paymentMethod.cardNumber.length - 4)}",
                 style: TextStyle(
-                  fontSize: 24.0,
-                  letterSpacing: 4.0,
-                  fontWeight: FontWeight.bold,
-                  color: paymentMethod.method == 'American Express' ? Colors.black : Colors.white
-                ),
+                    fontSize: 24.0,
+                    letterSpacing: 4.0,
+                    fontWeight: FontWeight.bold,
+                    color: paymentMethod.method == 'American Express'
+                        ? Colors.black
+                        : Colors.white),
               ),
             ),
             const SizedBox(height: 16.0),
@@ -193,38 +196,46 @@ class PaymentOptionsCard extends StatelessWidget {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Card Holder Name",
+                    Text(
+                      "Card Holder Name",
                       style: TextStyle(
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.bold,
-                        color: paymentMethod.method == 'American Express' ? Colors.black : Colors.white
-                      ),
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold,
+                          color: paymentMethod.method == 'American Express'
+                              ? Colors.black
+                              : Colors.white),
                     ),
-                    Text(paymentMethod.name,
+                    Text(
+                      paymentMethod.name,
                       style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                        color: paymentMethod.method == 'American Express' ? Colors.black : Colors.white
-                      ),
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: paymentMethod.method == 'American Express'
+                              ? Colors.black
+                              : Colors.white),
                     )
                   ],
                 ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Expiry Date",
+                    Text(
+                      "Expiry Date",
                       style: TextStyle(
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.bold,
-                        color: paymentMethod.method == 'American Express' ? Colors.black : Colors.white
-                      ),
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold,
+                          color: paymentMethod.method == 'American Express'
+                              ? Colors.black
+                              : Colors.white),
                     ),
-                    Text(paymentMethod.expirationDate,
+                    Text(
+                      paymentMethod.expirationDate,
                       style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                        color: paymentMethod.method == 'American Express' ? Colors.black : Colors.white
-                      ),
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: paymentMethod.method == 'American Express'
+                              ? Colors.black
+                              : Colors.white),
                     )
                   ],
                 )
@@ -237,15 +248,16 @@ class PaymentOptionsCard extends StatelessWidget {
   }
 }
 
-void toAddPaymentMethod(BuildContext context, List<PaymentModel> paymentMethodsList) async {
-
+void toAddPaymentMethod(
+    BuildContext context, List<PaymentModel> paymentMethodsList) async {
   // Navigator.pushReplacement(
   //     context, MaterialPageRoute(builder: (context) => AddPaymentMethod(curPayments: paymentMethodsList)));
 
   Navigator.pushReplacement(
-      context, MaterialPageRoute(builder: (context) => BlocProvider(
-        create: (context) => sl<ProfileEditCubit>(),
-        child: AddPaymentMethod(curPayments: paymentMethodsList),
-      )));
-
+      context,
+      MaterialPageRoute(
+          builder: (context) => BlocProvider(
+                create: (context) => sl<ProfileEditCubit>(),
+                child: AddPaymentMethod(curPayments: paymentMethodsList),
+              )));
 }
